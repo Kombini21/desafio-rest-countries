156aebf5e22787dbed772f3b28fc588542bc8d1e
Gabriel Terrazas <95561135+Kombini21@users.noreply.github.com>
2022-08-23 09:30:23 -0400

    Style the worksheet

--- app.js
+++ app.js
@@ -14,13 +14,33 @@ var headingColumnNames = [
     'Currencies'
 ]
 
+// Define all the styles of the worksheet 
+var styleHeader = wb.createStyle({
+    alignment: {
+        horizontal : 'center'
+    },
+    font: {
+        color: '#4F4F4F',
+        size: 16,
+        bold: true
+    }
+})
+
+var styleColumnHeadings = wb.createStyle({
+    font: {
+        color: '#808080',
+        size: 12,
+        bold: true
+    }
+})
+
 // Make the first header
-ws.cell(1,1,1,4,true).string('Countries List')
+ws.cell(1,1,1,4,true).string('Countries List').style(styleHeader)
 
 // Make all the column headings
 let headingColumnIndex = 1;
 headingColumnNames.forEach(element => {
-    ws.cell(2, headingColumnIndex++).string(element)
+    ws.cell(2, headingColumnIndex++).string(element).style(styleColumnHeadings)
 })
 
 
